8d5610f2c10095cc342fa0539890a3e323550afe
OmkarGambhir <gambhiromkar@gmail.com>
2024-06-28 02:35:15 +0530

    final commit

---
+++ src/constants.js
@@ -0,0 +1,135 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Congratulations, Adventurer,
+
+You've found the second chest! Inside, you'll discover more about me and my interests:
+
+I am interested in Blender and 3D arts, spending much of my time watching YouTube tutorials on how to create intricate 3D models. Additionally, I've made significant contributions to Club projects. One of my most memorable experiences was working with a team to recreate my entire SRM college in Unreal Engine. In my free time, I am fond of design work and love editing videos using Adobe After Effects, where I can bring my creative ideas to life. I made my intro using Adobe After Effects.
+
+I love to imagine creative things, and my mind is often full of these ideas.
+
+Just above where you found the first chests, hidden nearby, there lies a secret. Look for a golden bag where I keep something important to me. To discover more about my intro and what I've worked on, head towards the golden bag.
+
+If you'd like to explore further, here is my <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Github</a> where you can see some of my projects and contributions. Feel free to take a look!
+
+Best regards,
+
+Omkar Gambhir
+
+"As you finish reading, you get to know more about Omkar Gambhir. With the hint of the golden bag in mind, you set off to uncover more about his aspirations and secrets hidden within the Enchanted Forest."`,
+
+
+
+
+
+"cs-degree": `Congratulations, Adventurer,
+
+You've found the golden bag! Within its folds, you discover a small note awaiting your curious eyes:
+
+Dear Adventurer,
+
+It fills me with joy that you've ventured this far into the enchanted forest. Here, amidst the whispering leaves and ancient trees, I've hidden glimpses of who I am and what inspires me.
+
+Inside this note, hidden within th<a href="https://drive.google.com/drive/folders/1YJgwNHBFpw11NMWy9Tq3e-P6jjM8QCHv?usp=sharing" target="_blank">e</a> text, you'll find links that lead to discoveries about my passions and creations<a href="https://drive.google.com/file/d/1VCUt-zvdeY9L-ahPoyPyHvvd1zZXjixK/view?usp=drive_link" target="_blank">󠁣󠁣󠁣:</a>
+
+A mysterious key, said to unlock secrets beyond imagination.
+A potion, brewed with the essence of creativity and dreams.
+A solemn tombstone, a reminder of journeys past.
+Another chest, nestled in the far reaches of the forest, where two windmills stand tall in the distance.
+These are the pieces of my journey, crafted with love and creativity. I invite you to explore and understand the world that fuels my imagination.
+
+Best regards,
+
+Omkar Gambhir
+
+"As you read words penned by Omkar Gambhir, a sense of mystery deepens. You realize that beyond the quests and treasures, this journey has unveiled a glimpse into someone's dreams and aspirations, hidden within the enchanting depths of the forest.`,
+
+
+
+
+
+
+
+  "sofa-table": `you notice a wooden chest covered in moss. As you approach and open it, you find a weathered scroll inside. Unfurling it, you begin to read the elegant, handwritten script: 
+
+Greetings, Adventurer!
+
+Welcome to the Enchanted Forest. I am Omkar Gambhir, a CSE student specializing in software engineering and a part-time explorer. My journey began with tales of this mystical place, where magic and adventure intertwine.
+
+As a software engineering student, I bring a logical mind and problem-solving skills to navigate and understand the ancient runes and secrets of this forest. My bond with technology and nature allows me to uncover hidden paths and allies within these woods.\n
+
+You may find more letters like this one hidden in unexpected places, such as keys, potions, graveyards, or other chests. Each letter will tell you more about me, like a portfolio of my adventures and skills.
+
+Should you need guidance, seek me out. The forest and I are here to aid your journey.
+
+"As you finish reading, a warm breeze rustles the leaves around you, almost as if the forest itself is welcoming you. As you venture deeper into the forest, you come across a quiet graveyard. Here, you find another letter"`,
+
+
+
+  tv: `Hello again, Adventurer,
+
+In my spare time, I find solace in exploring the vast world of YouTube. I often watch videos about animations and 3D modeling, marveling at the creativity and skill of the artists. From these videos, I learn so many new things and often find myself imagining exploring the environments they create.
+
+I got into 3D modeling when the pandemic started, and it has been a fascinating journey ever since. It allowed me to express my creative thoughts on a canvas and bring my imagination to life. This very game world you are exploring now was created by me, a testament to my passion and dedication.
+
+Oh, and if you’re wondering why you found this letter in a graveyard, it’s because I spend so much time sitting in one place, watching and learning from YouTube, that I’m practically part of the undead now. Hence, the bones and the graveyard! But hey, at least I’m a knowledgeable skeleton.
+
+Before you continue, let me give you a hint: near the heart of this forest, between a towering green tree and a smaller sapling, another chest awaits you. Inside, you'll find a wealth of information about my interests, inspirations, and dreams.
+
+Continue your journey, and you may discover more about my passions and interests scattered throughout the forest.
+
+Reagrads,
+
+Omkar Gambhir
+
+"With this new piece of insight and a playful hint in mind, you continue your adventure, eager to uncover more about the mysterious and humorously undead Omkar Gambhir."`,
+
+
+
+
+  bed: `Inside are my creations and thoughts. They're not just for inspiration, but for sharing with friends who want to collaborate.
+
+Feel free to share your ideas with me too. Let's build a friendship and create together.
+
+And Congratulations on reaching the end of this journey. Take a moment to explore the forest, relax, and enjoy the tranquility away from the fast-paced world.
+
+When you're ready to leave, head straight back to the first chest where you began. There, you'll find the door that leads you out of the forest.
+
+Thank you for exploring with me.
+
+Best regards,
+
+Omkar
+
+`,
+
+
+
+
+  resume: `Dear Explorer,
+
+Congratulations on finding the key to new opportunities. Attached is my <a href="https://drive.google.com/file/d/10sVYaycu3G3jw_LRim_6acUTGyoz2HCR/view?usp=drive_link" target="_blank">resume</a>, a golden key to job opportunities and a better way to present myself:
+
+Explore my journey, skills, and aspirations that shape my path.
+
+Best regards,
+
+Omkar
+
+"As you explore further, your eyes catch sight of a solemn tombstone nearby. Intrigued, you approach and read the inscription:`,
+
+
+
+
+  projects: `*Here lies the pathway to more of Omkar's journey and his creations. For more about my journey, and my creativity and programming please <a href="https://github.com/OmkarGambhir" target="_blank">tag/dig</a> here*
+  "Beside the tombstone, a shimmering potion vial catches their eye. Curiosity piqued, you pick it up and examine the label:" `,
+
+
+
+  library: `"This potion symbolizes Omkar's passion for creativity and learning. In my free time, I immerse myself in exploring various novels, comics, and designs. Each story and artwork fuels my imagination and keeps me inspired and up-to-date."
+  "Next to the potion, there's a chest hidden under a tree."`,
+
+  exit: `As you reach the exit, I bid you farewell. Feel free to close the tab whenever you're ready to depart from this enchanted realm.`,
+
+};
